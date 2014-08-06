fb182fd78d440fe62cf37352565e7ccc7935d1cf
deepujohn <deepujohn.ideology@gmail.com>
2014-08-06 12:39:10 +0530

    Make a basic User model (including secure passwords)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe "User pages" do
-  subject { page }
-  let(:full_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }  
   describe "signup page" do
     before { visit signup_path }
     it { should have_content('Sign up') }
-    it { should have_title("#{full_title} | Sign up")}
+    it { should have_title(full_title "Sign up")}
   end
 end
